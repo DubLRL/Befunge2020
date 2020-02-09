ca4ee8c9a918bb3974afc967026baf880f1d39e0
Thomas <thomas@thomas.com>
2020-02-09 12:30:00 -0800

    h

--- src/main/java/com/notgull/befunge2020/Grid.java
+++ src/main/java/com/notgull/befunge2020/Grid.java
@@ -1,23 +1,26 @@
 package com.notgull.befunge2020;
 
 public class Grid {
-    protected long x;
-    protected long y;
+    protected int x, x1;
+    protected int y, y1;
+    protected String[][] grid;
     public Grid(String fileName, int x, int y){
-
+        //get size of the array fileName and pass into grid
+        
+        grid = new String[x1][y1];
         this.x = x;
         this.y = y;
     }
-    protected void resetX(long x){
+    protected void setX(int x){
         this.x = x;
     }
-    protected void resetY(long y){
+    protected void setY(int y){
         this.y = y;
     }
-    public long getX(){
+    public int getX(){
         return this.x;
     }
-    public long getY(){
+    public int getY(){
         return this.y;
     }
 }
